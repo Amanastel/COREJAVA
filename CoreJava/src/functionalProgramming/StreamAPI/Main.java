a6aecc01f976cfd562584d427bac144bac15e550
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-07-24 01:31:07 +0530

    Functional Interface

---
+++ CoreJava/src/functionalProgramming/StreamAPI/Main.java
@@ -0,0 +1,38 @@
+package functionalProgramming.StreamAPI;
+
+public class Main {
+    public static void main(String[] args) {
+        // StreamAPI
+        // StreamAPI is a new feature in Java 8. It is used to process a collection of objects.
+        // It is used to iterate, filter and extract data from collection.
+        // It is used to perform aggregate operations on data returned from collections.
+        // It is used to return a stream of data from a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+        // It is used to return a stream of data based on the condition applied on a collection.
+
+        // StreamAPI is used to process a collection of objects.
+        // StreamAPI is used to iterate, filter and extract data from collection.
+        // StreamAPI is used to perform aggregate operations on data returned from collections.
+        // StreamAPI is used to return a stream of data from a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+        // StreamAPI is used to return a stream of data based on the condition applied on a collection.
+
+        // StreamAPI is used to process a collection of objects.
+        // StreamAPI is used to iterate, filter and extract data from collection.
+        // StreamAPI is used to perform aggregate operations on data returned from collections.
+        // StreamAPI is used to return a stream of data from a collection.
+        // StreamAPI is used to return
+
+
+    }
+}
